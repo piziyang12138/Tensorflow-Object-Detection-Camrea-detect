77736a1fd2c5da42296236c3bf1b5f815dcffcb6
piziyang12138 <18604051140@163.com>
2018-04-27 19:44:51 +0800

    first commit

---
+++ camrea_detect.py
@@ -0,0 +1,113 @@
+import multiprocessing
+import cv2
+import numpy as np
+import os
+import tensorflow as tf
+
+from threading import Thread
+from multiprocessing import Pool, Queue
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+PATH_TO_CKPT = os.path.join(
+    r'E:\TensorFlowObjectDetection\models\research\object_detection\ssd_mobilenet_v1_coco_2017_11_17'
+    r'\frozen_inference_graph.pb')
+PATH_TO_LABELS = os.path.join(
+    r'E:\TensorFlowObjectDetection\models\research\object_detection\data\mscoco_label_map.pbtxt')
+NUM_CLASSES = 90
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                            use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+
+def detect_objects(image_np, sess, detection_graph):
+    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+    # Each box represents a part of the image where a particular object was detected.
+    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+    # Each score represent how level of confidence for each of the objects.
+    # Score is shown on the result image, together with the class label.
+    scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    # Actual detection.
+    (boxes, scores, classes, num_detections) = sess.run(
+        [boxes, scores, classes, num_detections],
+        feed_dict={image_tensor: image_np_expanded})
+
+    # Visualization of the results of a detection.
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8)
+    return image_np
+
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+        sess = tf.Session(graph=detection_graph)
+
+
+def work(input_q, output_q):
+    while True:
+        frame = input_q.get()
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        output_q.put(detect_objects(frame_rgb, sess, detection_graph))
+
+
+class MyVideo:
+    def __init__(self):
+        self.stream = cv2.VideoCapture(0)
+        self.ret, self.frame = self.stream.read()
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        while True:
+            if self.stopped:
+                return
+            self.ret, self.frame = self.stream.read()
+
+    def stop(self):
+        self.stopped = True
+
+    def read(self):
+        return self.frame
+
+
+if __name__ == '__main__':
+    logger = multiprocessing.log_to_stderr()
+    logger.setLevel(multiprocessing.SUBDEBUG)
+    my_video = MyVideo().start()
+    input_q = Queue(5)
+    output_q = Queue(5)
+    pool = Pool(2, work, (input_q, output_q))
+    while True:
+        frame = my_video.read()
+        input_q.put(frame)
+
+        output_rgb = cv2.cvtColor(output_q.get(), cv2.COLOR_RGB2BGR)
+        cv2.imshow("capture", output_rgb)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    pool.terminate()
+    my_video.stop()
+    cv2.destroyAllWindows()
